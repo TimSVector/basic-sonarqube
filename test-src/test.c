607457e82c9c895adb310ae2cdf6cb98ad3b4317
TimSVector <tim.schneider@vector.com>
2023-10-18 13:47:02 -0400

    Updates

--- test-src/test.c
+++ test-src/test.c
@@ -16,7 +16,21 @@ int test_001(void)
     return ret_val;
 }
 
+
+int test_002(void)
+{
+    int ret_val = func_001();
+    
+    if (ret_val > 1000)
+        printf("[PASS] test_001\n");
+    else
+        printf("[FAIL] test_001\n");
+        
+    return ret_val;
+}
+
 int main(void)
 {
     test_001();
+    test_002();
 }
